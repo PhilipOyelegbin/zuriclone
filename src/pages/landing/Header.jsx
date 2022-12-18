b4286a8ef4c80b36a5fd62fd55b15123e77cbb0d
Philip Oyelegbin <philipoyelegbin@gmail.com>
2022-12-18 11:47:41 +0100

    fix: internal links to pages

--- src/pages/landing/Header.jsx
+++ src/pages/landing/Header.jsx
@@ -9,8 +9,8 @@ const Header = () => {
         <h2 className='text-[26px] font-[500] mb-[36px]'>Learn, Build, Grow.</h2>
         <p className='mb-[16px]'>Unlock your Brillance with our hands-on <a href="#" className='inner-link'>beginner</a> and <a href="#" className='inner-link'>expert training</a>. Zuri Team has been immensely successful in creating a global network of a highly adept intelligent workspace that can help your company achieve their mission-critical <a href="#" className='inner-link'>projects and goals</a></p>
         <div className='flex flex-col gap-5 mt-[92px] mb-12 md:gap-10 md:mb-0 md:flex-row'>
-          <Link to="/zuriclone/training" className="btn bg-red-500 hover:bg-white text-white hover:text-red-500 border border-red-500">I'm new to the Industry</Link>
-          <Link to="/zuriclone/talent" className="btn bg-white hover:bg-red-500 text-red-500 border hover:text-white border-red-500">I need industry experience</Link>
+          <Link to="/training" className="btn bg-red-500 hover:bg-white text-white hover:text-red-500 border border-red-500">I'm new to the Industry</Link>
+          <Link to="/talent" className="btn bg-white hover:bg-red-500 text-red-500 border hover:text-white border-red-500">I need industry experience</Link>
         </div>
       </div>
     </header>
